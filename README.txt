b5ae503e9bc6760ef4cae23ad59750038b9f18f2
tyler <t.louks88@gmail.com>
2022-05-22 15:50:23 -0600

    upload mod

---
+++ README.txt
@@ -0,0 +1,3 @@
+i made this for my omnifactory playthrough
+
+do whatever you want with the code i don't care
---
+++ src/main/java/dev/ty/aggro/Aggro.java
@@ -0,0 +1,51 @@
+package dev.ty.aggro;
+
+import net.minecraft.server.management.PlayerList;
+import net.minecraft.world.GameRules;
+import net.minecraft.world.WorldServer;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.GameRuleChangeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.UUID;
+
+@Mod(modid = Aggro.MOD_ID)
+public class Aggro {
+    public static final String MOD_ID = "aggro";
+    public static boolean defaultValue = false;
+
+    private static final String ruleName = "disableMobAggro";
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @Mod.EventHandler
+    public void serverStart(FMLServerStartingEvent event) {
+        WorldServer world = event.getServer().worlds[0];
+        GameRules rules = world.getGameRules();
+
+        if (!rules.hasRule(ruleName))
+            rules.addGameRule(ruleName, String.valueOf(defaultValue), GameRules.ValueType.BOOLEAN_VALUE);
+
+        setValue(event.getServer().getPlayerList(), world.getGameRules());
+    }
+
+    @SubscribeEvent
+    public void gameRuleChanged(GameRuleChangeEvent event) {
+        if (!event.getRuleName().equals(ruleName)) return;
+
+        setValue(event.getServer().getPlayerList(), event.getRules());
+    }
+
+    private void setValue(PlayerList list, GameRules rules) {
+        list.getPlayers()
+                .stream()
+                .filter(player -> player.getGameProfile().getId().equals(UUID.fromString("1d5e02e0-7e54-4e9e-8d9c-548b22c02daf")))
+                .forEach(player -> player.capabilities.disableDamage = rules.getBoolean(ruleName));
+    }
+}
